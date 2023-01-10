1210ea8d621c6462321a522c4779013c0f3dad5e
EUNSEO814 <eunseo814@gmail.com>
2023-01-10 14:14:57 +0900

    🚚 코드 나누기

--- typeScript.ts
+++ typeScript.ts
@@ -1,23 +1,3 @@
-let a = "hello";
-
-a = "bye";
-
-// a = 1
-//a 방법 = 타입을 추론하게 하는 방법
-
-// let b : boolean = "x"
-//b는 boolean이어야 한다는 것을 명시적으로 지정하는 문법
-
-let b: boolean = false;
-
-//c === number[]
-// let c = [1, 2, 3]
-
-// c.push("1")
-
-let c: number[] = [];
-c.push(1);
-
 // 간단한것은 타입스크립트가 추론하게 하는 방법이 좋음. 코드가 짧아짐.
 const player = {
   name: "kim",
---
+++ typeScript1.ts
@@ -0,0 +1,19 @@
+let a = "hello";
+
+a = "bye";
+
+// a = 1
+//a 방법 = 타입을 추론하게 하는 방법
+
+// let b : boolean = "x"
+//b는 boolean이어야 한다는 것을 명시적으로 지정하는 문법
+
+let b: boolean = false;
+
+//c === number[]
+// let c = [1, 2, 3]
+
+// c.push("1")
+
+let c: number[] = [];
+c.push(1);
