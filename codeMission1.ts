d69a63f6a402afe87aec58e38d3aada473e1b3ad
EUNSEO814 <eunseo814@gmail.com>
2023-01-13 17:02:22 +0900

    ⚡️ code Challenge

--- codeMission1.ts
+++ codeMission1.ts
@@ -23,21 +23,44 @@
 
 // 수정본
 
-type Last = <T>(arr: T[]) => T;
+type Last1 = <T>(arr: T[]) => T;
 
-const last: Last = (arr) => {
+const last1: Last1 = (arr) => {
   return arr[arr.length - 1];
 };
-const a = last([1, 2, 3]);
-const b = last(["a", "b", "c"]);
+const a = last1([1, 2, 3]);
+const b = last1(["a", "b", "c"]);
 
-type Prepend = <T>(arr: T[], item: T) => T[];
+type Prepend1 = <T>(arr: T[], item: T) => T[];
 
-const prepend: Prepend = (arr, item) => {
+const prepend1: Prepend1 = (arr, item) => {
   const items = [...arr];
   return [item, ...items];
 };
 
-const c = prepend([1, 2, 3], 4);
-const d = prepend([true, false, true], true);
-const e = prepend(["a", "b", "c"], "d");
+const c = prepend1([1, 2, 3], 4);
+const d = prepend1([true, false, true], true);
+const e = prepend1(["a", "b", "c"], "d");
+
+//답
+// Last
+
+type Last = <T>(items: T[]) => T;
+
+const last: Last = (items) => items[items.length - 1];
+
+const lastItem = last([1, 2, 3, 4, 5]);
+
+console.log(lastItem);
+
+// Prepend
+
+type Prepend = <T>(items: T[], item: T) => T[];
+
+const prepend: Prepend = (items, item) => [item, ...items];
+
+const items = [1, 2, 3, 4, 5];
+
+const newItems = prepend(items, 0);
+
+console.log(newItems);
